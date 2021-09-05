7c183fca17522adf4cc7be60c693ba49a5ff19a8
casko1 <47791924+casko1@users.noreply.github.com>
2021-09-05 02:24:50 +0200

    Edited command description

--- src/main/java/com/casko1/wheelbarrow/commands/music/BassboostCommand.java
+++ src/main/java/com/casko1/wheelbarrow/commands/music/BassboostCommand.java
@@ -5,8 +5,8 @@ public class BassboostCommand extends FilterCommand{
     public BassboostCommand(){
         super("bassboost");
         this.name = "bassboost";
-        this.help = "**Applies timescale filter to current track.** *Example: $$timescale speed 1.2*";
-        this.arguments = "<speed | pitch | rate> <number>";
+        this.help = "**Applies bass boost filter to current track.** *Example: $$bassboost gain 4*";
+        this.arguments = "gain <number> [0-10]";
         this.aliases = new String[]{"bb"};
         this.guildOnly = false;
     }
